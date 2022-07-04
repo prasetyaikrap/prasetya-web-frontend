a62db05d9854f0bab9600eb84e66bc9e3bb3f47b
Prasetya Ikra Priyadi <prasetya.ikrapriyadi@gmail.com>
2022-07-04 09:02:11 +0700

    [Major Changes] First Commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "../styles/globals.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,4 @@
+export default function Home() {
+  const num = 12;
+  return <h1>Hello World {num}</h1>;
+}
