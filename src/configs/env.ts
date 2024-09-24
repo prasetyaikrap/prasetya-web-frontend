6fbab0b2fa0e1860b763c486b62d36e5de8b3254
prasetyaikrapriyadi <prasetya.ikra@efishery.com>
2024-09-24 15:07:19 +0700

    master(): add app_env

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -9,4 +9,5 @@ export const ENV = {
   JWT_SECRET_REFRESH_TOKEN_KEY: process.env.JWT_SECRET_REFRESH_TOKEN_KEY ?? "",
   APP_VERSION: process.env.NEXT_PUBLIC_APP_VERSION ?? "dev-environment-v0.0.0",
   APP_ID: process.env.NEXT_PUBLIC_APP_ID ?? "",
+  APP_ENV: process.env.NEXT_PUBLIC_APP_ENV ?? "dev",
 };
